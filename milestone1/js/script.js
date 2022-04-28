8c8a95d30283ec31267f8e5a2091cd1c37e8c0c1
katy01101011 <katy.91560@gmail.com>
2022-04-28 13:29:19 +0200

    add: milestone1 file

---
+++ milestone1/js/script.js
@@ -0,0 +1,43 @@
+const team = [
+    {
+      name: 'Wayne Barnett',
+      role: 'Founder & CEO',
+      image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+      name: 'Angela Caroll',
+      role: 'Chief Editor',
+      image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+      name: 'Walter Gordon',
+      role: 'Office Manager',
+      image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+      name: 'Angela Lopez',
+      role: 'Social Media Manager',
+      image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+      name: 'Scott Estrada',
+      role: 'Developer',
+      image: 'scott-estrada-developer.jpg',
+    },
+    {
+      name: 'Barbara Ramos',
+      role: 'Graphic Designer',
+      image: 'barbara-ramos-graphic-designer.jpg',
+    },
+  ];
+
+//   MILESTONE 1:
+// stampare su console le informazioni di nome, ruolo e la stringa della foto
+
+for (let i = 0; i < team.length; i++) {
+    const teamMembers = team[i];
+
+    console.log(teamMembers.name);
+    console.log(teamMembers.role);
+    console.log(teamMembers.image);
+}
